50d8cba3922df3c854d1e9391c7d381cfd1aaf23
Ethan Li <lietk12@gmail.com>
2020-01-29 14:20:36 -0800

    Improved platform deduction.

--- src/Phyllo/Platform.h
+++ src/Phyllo/Platform.h
@@ -10,12 +10,22 @@
 #define PHYLLO_PLATFORM_ATMELSAM 2
 #define PHYLLO_PLATFORM_TEENSY 3
 
+// Deduce platform from Arduino framework
 #ifndef PHYLLO_PLATFORM
-
-#if defined (__AVR__) || (__avr__)
+#if defined (ARDUINO_ARCH_AVR)
 #define PHYLLO_PLATFORM PHYLLO_PLATFORM_ATMELAVR
+#elif defined (ARDUINO_ARCH_SAM)
+#define PHYLLO_PLATFORM PHYLLO_PLATFORM_ATMELSAM
 #elif defined (CORE_TEENSY)
 #define PHYLLO_PLATFORM PHYLLO_PLATFORM_TEENSY
+#endif
+#endif
+
+// Attempt to automatically deduce platform if defines from Arduino framework are missing
+#ifndef PHYLLO_PLATFORM
+#if defined (__AVR__) || (__avr__)
+#define PHYLLO_PLATFORM PHYLLO_PLATFORM_ATMELAVR
+#pragma warn("Assuming platform is ATMELAVR!")
 #elif defined (__arm__)
 #define PHYLLO_PLATFORM PHYLLO_PLATFORM_ATMELSAM
 #pragma warn("Assuming platform is ATMELSAM!")
@@ -23,16 +33,16 @@
 #define PHYLLO_PLATFORM PHYLLO_PLATFORM_UNKNOWN
 #pragma warn("Unknown platform!")
 #endif
-
 #endif
 
+// Set default values for platforms
 #if PHYLLO_PLATFORM == PHYLLO_PLATFORM_ATMELAVR
 #define ETL_NO_STL
 #ifndef PHYLLO_TRANSPORT_CHUNK_SIZE_LIMIT
 #define PHYLLO_TRANSPORT_CHUNK_SIZE_LIMIT 127
 #pragma warn("Serial frames are limited to 127 bytes instead of 255 bytes!")
 #endif
-// Polyfills for missing features needed by ETL on AVR 8-bit microcontrollers
+// Polyfills for missing features needed by ETL on AVR 8-bit microcontrollers which aren't provided by AVR or ArduinoSTL
 namespace std {
   template <typename T>
   T* addressof(T& t) {
@@ -50,4 +60,5 @@ namespace std {
 #define ETL_NO_STL
 #endif
 
+// Define macros for easier debugging
 #define PHYLLO_DEBUG_LED digitalWrite(LED_BUILTIN, HIGH);
